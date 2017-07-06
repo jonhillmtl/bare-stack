8aa7b454fd14c1877e142721c63c6b23adf582d0
Jon Hill <jon@jonhill.ca>
2017-07-06 13:22:05 +0200

    initial commit

---
+++ bare_stack/__init__.py
@@ -0,0 +1,14 @@
+import pprint
+import inspect
+
+def bare_stack():
+    for frame in inspect.stack():
+        args, varargs, varkw, locals = inspect.getargvalues(frame.frame)
+        args = inspect.formatargvalues(args, varargs, varkw, locals)
+        
+        print("{} ({}): {}({})".format(
+            frame.filename, 
+            frame.lineno, 
+            frame.function, 
+            args))
+    
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='bare-stack',
+      version='0.1',
+      description='Python function to display a stack that I find easy to read.',
+      author='Jon Hill',
+      author_email='jon@jonhill.ca',
+      url='https://github.com/jonhillmtl/bare-stack',
+      packages = ['bare_stack'],
+      license='MIT'
+)
